6249b4c9e034c89ff113fd9df6a835bceee71db7
mauricio <mauricio.p.ninomiya@accenture.com>
2024-03-07 11:07:28 -0300

    Fix - better treatment for promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,7 +45,7 @@ public class Program {
 					String type = sc.nextLine().toUpperCase();
 					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
 						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
-						type = sc.nextLine().toUpperCase();
+						type = sc.nextLine().toUpperCase(); 
 					}
 					chessMatch.replacePromotedPiece(type);
 				}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -148,7 +148,7 @@ public class ChessMatch {
 			throw new IllegalStateException ("There is no piece to be promoted");
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			return promoted;
+			return promoted; 
 		}
 		
 		Position pos  = promoted.getChessPosition().toPosition();
